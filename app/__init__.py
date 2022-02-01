d56c5e665da88c27622476c0f41fcc2d8aeaabed
trevin-livele <liveletrevin6@gmail.com>
2022-02-01 10:05:08 +0300

    stored movie base url

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from.config import DevConfig
 
 #initializing the application
-app = Flask(__name__)
+app = Flask(__name__ , instance_relative_config=True)
 
 
 #setting up configuration
---
+++ app/config.py
@@ -0,0 +1,7 @@
+class Config:
+    '''
+General configuration parent class
+
+    '''
+
+    NEWS_API_BASE_URL =  "https://newsapi.org/v2/everything?q=Apple&from=2022-01-26&sortBy=popularity&apiKey=ffb788ce77dc47d3aa4d36f8cccc8f43"
